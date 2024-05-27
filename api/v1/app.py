5ac6ad9f57be82638d9aa2679f68d08a93657ef1
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-05-27 20:16:05 +0200

    task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-""" app """
-from flask import Flask
+""" runs flask and imports blueprint """
+
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """ the Api status"""
+def teardown(exception):
+    """ close storage session"""
     storage.close()
 
-@web_app.errorhandler(404)
-def error_pages_404(error):
-    """ errors that returns a JSON-formatted status code"""
-    return jsonify((error='Not found'), 404)
 
-if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+@app.errorhandler(404)
+def error_404(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
