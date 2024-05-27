bb43ae1e3fef474aae0793a0d4afed8ecb4cdb63
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-05-27 20:23:17 +0200

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,21 @@ from models import storage
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     return jsonify(status="OK")
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    endpoint that retrieves the number of each objects by type
+    """
+    cls_objs = {
+        "users": storage.count("User"),
+        "states": storage.count("State"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "amenities": storage.count("Amenity"),
+        "reviews": storage.count("Review"),
+    }
+    counter = jsonify(cls_objs)
+    counter.status_code = 200
+    return counter
